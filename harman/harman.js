a10d292d1a024fca6e2c09cbf6dbb6ef1efeffcc
harmanwd <147791415+harmanwd@users.noreply.github.com>
2023-10-25 06:17:28 +0000

    harman

--- harman/harman.js
+++ harman/harman.js
@@ -1,3 +1,16 @@
-const min = 1
-const max = 6
-console.log(Math.floor(Math.random()*(max -  min + 1)) + min);
+// const min = 1
+// const max = 6
+// console.log(Math.floor(Math.random()*(max -  min + 1)) + min);
+
+let favouritemovie = ["KGF","KGF2","MASTANEY","MONEYHEIST"];
+let movielist = ["SQUID GAME"]
+
+// console.log(movielist);
+
+console.log(favouritemovie.length);
+let addmovie = favouritemovie.concat(movielist);
+
+console.log(addmovie.splice(1,2));
+console.log(addmovie);
+
+// let removemovie = movielist.splice()
